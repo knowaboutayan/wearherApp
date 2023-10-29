d5d9e652c05ca649b30bd44f2c7a5e11088b0479
knowaboutayan <info.creationologt@gmail.com>
2023-10-29 15:40:10 +0530

    some other bugs fix

--- src/Components/pages/ServerIssue.js
+++ src/Components/pages/ServerIssue.js
@@ -1,7 +1,7 @@
 import "../Css/ErrorPage.css"
-const ServerIssue=()=><div className='errorBox flex'>
-<i class="fa fa-refresh" aria-hidden="true"></i>
+const ServerIssue = () => <div className='errorBox flex'>
+    <img src="https://cdn-icons-png.flaticon.com/512/5192/5192901.png" alt="" className="fa-refresh" width={"200px"}></img>
 
-<b> Please Wait...</b>
+    <b> Please Wait...</b>
 </div>
 export default ServerIssue;
