a19f0ed9a568ccec1f81556ed7561b7c19e0d1aa
AYAN PRADHAN <149263455+knowaboutayan@users.noreply.github.com>
2024-09-20 19:28:22 +0530

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,7 +3,7 @@ const Footer = () => {
         <div style={{ textAlign: "center" }}>
             <h6>
                 Designed and developed with <i className="fa fa-heart"></i> by
-                <br /> &copy; <a href="https://knowaboutayan.000webhostapp.com" rel="noreferrer" target="_blank">Cretionology</a>
+                <br /> &copy; <a href="https://linkedin.com/in/ayan-pradhan-160471252" rel="noreferrer" target="_blank">Cretionology</a>
             </h6>
         </div>
     );
